c6a33d4e6286c719a1da348b2b9f3b3a28ac25ec
liuyu7177 <853469204@qq.com>
2019-05-14 16:08:08 +0800

    idea 昨日未来 java web 项目提交

--- src/main/java/com/liuyu7177/zuoriweilai/web/HomeController.java
+++ src/main/java/com/liuyu7177/zuoriweilai/web/HomeController.java
@@ -1,7 +1,30 @@
 package com.liuyu7177.zuoriweilai.web;
 
+import com.liuyu7177.zuoriweilai.model.entity.Seckill;
+import com.liuyu7177.zuoriweilai.service.SeckillService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
+
 /**
  * Created by liuyu7177 On 2019/5/13
  */
+@Controller
 public class HomeController {
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+    @Autowired
+    private SeckillService seckillService;
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String list(Model model) {
+        List<Seckill> list = seckillService.getSeckillList();
+        model.addAttribute("list", list);
+        return "list";
+    }
 }
